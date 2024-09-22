e291098268ea7701120bef49f14bc3d8ed0ee6ac
LOKESHKARMOKAR <146501836+LOKESHKARMOKAR@users.noreply.github.com>
2024-09-22 08:40:34 +0530

    testing Framework by jasmine

---
+++ tests/cheackout/orderSummaryTest.js
@@ -0,0 +1,10 @@
+import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
+describe('test suits: renderOrderSummary',()=>{
+    it('display the cart', ()=>{
+        document.querySelector('.js-test-container').innerHTML=
+        `
+        <div class="js-order-summary"></div>`;
+
+        spyOn();
+    });
+});
